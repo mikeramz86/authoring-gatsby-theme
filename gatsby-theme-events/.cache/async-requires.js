b552f2b5dc985f74d3e75b6139004a8a0672e12f
Mike <mikeramzdesign@gmail.com>
2019-07-06 14:56:07 -0700

    init commit

---
+++ gatsby-theme-events/.cache/async-requires.js
@@ -0,0 +1,7 @@
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+exports.components = {
+  "component---cache-dev-404-page-js": () => import("/Users/michaelramirez/Desktop/egghead/authoring-gatsby-themes/gatsby-theme-events/.cache/dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */)
+}
+
